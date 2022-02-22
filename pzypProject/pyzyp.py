46af0bf4f294485996fbc418a19658048500f621
alexdyaz <84355113+alexdyaz@users.noreply.github.com>
2022-02-22 21:20:25 +0000

    Code cleanup

--- pzypProject/pyzyp.py
+++ pzypProject/pyzyp.py
@@ -15,6 +15,8 @@ __descricao__ = 'LZSS Encoder/ Decoder'
 # =============================================================================
 
 from encodings import utf_8
+import os
+import subprocess
 import io
 import math
 import bitstruct
@@ -54,12 +56,6 @@ MAX_STRING_SIZE = 2 ** ENCODED_LEN_SIZE - 1 + MIN_STRING_SIZE  # in bytes
 
 
 class PZYPContext:
-	"""
-	A context objects stores related parameters that control LZSS
-	algorithm. It's essentially a data class that in future versions
-	will probably be defined with the new @dataclass decorator available
-	since Python 3.7.
-	"""
 
 	def __init__(
 			self,
@@ -67,67 +63,44 @@ class PZYPContext:
 			encoded_len_size=ENCODED_LEN_SIZE,
 			unenc_string_size=UNENCODED_STRING_SIZE,
 	):
-		"""
-		These sizes are specified in bits.
-		"""
 		self._encoded_offset_size = encoded_offset_size
 		self._encoded_len_size = encoded_len_size
 		self._unencoded_string_size = unenc_string_size
 
-	#:
 	@property
 	def encoded_offset_size(self):
 		return self._encoded_offset_size
 
-	#:
 	@property
 	def encoded_len_size(self):
 		return self._encoded_len_size
 
-	#:
 	@property
 	def unencoded_string_size(self):
 		return self._unencoded_string_size
 
-	#:
 	@property
 	def encoded_string_size(self) -> int:
 		return self.encoded_offset_size + self.encoded_len_size  # in bits
 
-	#:
 	@property
 	def window_size(self) -> int:
 		return 2 ** self.encoded_offset_size  # in bytes
 
-	#:
 	@property
 	def break_even_point(self) -> int:
 		return self.encoded_string_size // 8  # in bytes
 
-	#:
 	@property
 	def min_string_size(self) -> int:
 		return self.break_even_point + 1  # in bytes
 
-	#:
 	@property
 	def max_string_size(self) -> int:
 		return 2 ** self.encoded_len_size - 1 + self.min_string_size  # in bytes
 
 
-#:
-
-
-#:
-
 class LZSSWriter:
-	"""
-	An LZSSWriter object writes encoded or unencoded strings to an
-	output stream, as stated in the classic LZSS specification.
-	A string is just a pair <offset/postion, length>, while an
-	unencoded string is a bytes object with (most probably) one
-	byte.
-	"""
 
 	def __init__(
 			self,
@@ -146,13 +119,11 @@ class LZSSWriter:
 		if (ctx.encoded_offset_size + ctx.encoded_len_size) % 8 == 0:
 			self._inner_bitify_enc = self._bitify_enc_multiple_of_8
 
-	#:
 	def write(self, data: Union[bytes, Tuple[int, int]]):
 		(self._bitify_unenc if isinstance(data, bytes) else self._bitify_enc)(data)
 		if len(self.buffer) == 32_768:
 			self._stream_bits()
 
-	#:
 	def _bitify_enc(self, enc_data: Tuple[int, int]) -> int:
 		pos, len_ = enc_data
 		ctx = self._ctx
@@ -163,64 +134,46 @@ class LZSSWriter:
 		self._inner_bitify_enc(pos, mapped_len)
 		return ctx.encoded_string_size  # remember: it's in bits ...
 
-	#:
 	def _bitify_enc_not_multiple_of_8(self, pos: int, mapped_len: int):
 		enc_fmt = self._enc_fmt
 		bitarr = bitarray()
 		bitarr.frombytes(enc_fmt.pack(pos, mapped_len))
 		self.buffer.extend(bitarr[:enc_fmt.calcsize()])
 
-	#:
 	def _bitify_enc_multiple_of_8(self, pos: int, mapped_len: int):
 		self.buffer.frombytes(self._enc_fmt.pack(pos, mapped_len))
 
-	#:
 	def _bitify_unenc(self, unenc_data: bytes) -> int:
 		assert unenc_data
 		self.buffer.append(False)
 		self.buffer.frombytes(unenc_data)
 		return self._ctx.unencoded_string_size
 
-	#:
 	def _stream_bits(self, size_in_bits: int = 2 ** 63 - 1):
 		data = self.buffer[:size_in_bits].tobytes()
 		del self.buffer[:size_in_bits]
 		self._out.write(data)
 
-	#:
 	def close(self, flush_buffer=True):
 		if flush_buffer:
 			self._flush_buffer()
 		if self._close_out_stream:
 			self._out.close()
 
-	#:
 	def __enter__(self):
 		return self
 
-	#:
 	def __exit__(self, exc_type, exc_value, traceback):
 		# Flush the buffer only if an exception hasn't occurred
 		self.close(not exc_value)
 
-	#:
 	def _flush_buffer(self):
 		if self.buffer:
 			self.buffer.fill()  # pad buffer with 0s to get
 			self._stream_bits()  # an integral number of bytes
 
 
-#:
-
-
-#:
-
 class LZSSReader:
-	"""
-	A LZSS decoder can use this class to create an object that parses
-	reads the 'in_' stream in termos of encoded or unencoded string,
-	accordingly to the classic LZSS specification.
-	"""
 
 	def __init__(
 			self,
@@ -236,7 +189,6 @@ class LZSSReader:
 			f'u{ctx.encoded_offset_size}u{ctx.encoded_len_size}'
 		)
 
-	#:
 	def read(self):
 		ctx = self._ctx
 		assert len(self.buffer) <= ctx.encoded_string_size + 8
@@ -260,11 +212,8 @@ class LZSSReader:
 			return True, (pos, len_ + ctx.min_string_size)
 		return False, data.tobytes()
 
-	#:
 	def _end_of_data(self) -> bool:
-		# Remember: The input stream may be at eof, but the buffer may
-		# still have 0's. Why? Padding to fill the last byte when
-		# encoding.
+
 		buffer_maybe_empty = (
 				not self.buffer or
 				(self.buffer.count(0) == len(self.buffer) and len(self.buffer) < 8)
@@ -274,18 +223,15 @@ class LZSSReader:
 		self._in.seek(curr_pos)
 		return eof
 
-	#:
 	def __iter__(self):
 		return self
 
-	#:
 	def __next__(self):
 		data = self.read()
 		if not data[1]:
 			raise StopIteration()
 		return data
 
-	#:
 	def close(self):
 		assert len(self.buffer) < 16
 		empty_buffer = not self.buffer or self.buffer.count(0) == len(self.buffer)
@@ -296,24 +242,15 @@ class LZSSReader:
 		if self.close_in_stream:
 			self._in.close()
 
-	#:
-
 	def __enter__(self):
 		return self
 
-	#:
 	def __exit__(self, exc_type, exc_value, traceback):
 		self.close()
 
-	#:
 	class UnreadData(Exception):
 		pass
 
-
-#:
-
-
-#:
 
 def textChar_elements(textChar_verify2, buffer2):
 	textChar_count = 0
@@ -420,7 +357,7 @@ def decode(in_: BinaryIO, out: BinaryIO, lzss_reader=None, ctx=PZYPContext()):
 		var_bytes = b''
 
 		for encoded_flag, elemento in lzss_in:
-			# print("Elemento = ", elemento)
+
 			if encoded_flag:
 				prefix_pos, prefix_len = elemento
 				actualText = output[-prefix_pos:][:prefix_len]
@@ -444,7 +381,7 @@ def _main():
 	args = docopt(usage)
 	var_bytes = b''
 	if args['-d']:
-		# print("decompress")
+
 		with open(args['<file_name>'], 'rb') as in_:
 			with open('decompressed.txt', 'wb') as out_1:
 				var_bytes += decode(in_, out_1)
@@ -452,7 +389,11 @@ def _main():
 			print('Decompress #: O ficheiro de saída tem os seguintes dados: ')
 			with open('decompressed.txt', 'rb') as out_2:
 				dados_comp = out_2.read()
-			print(dados_comp)
+				print(dados_comp)
+				try:
+					subprocess.call([os.system('decompressed.txt')])
+				finally:
+					return
 
 	elif args['-c']:
 		# print("compress")
